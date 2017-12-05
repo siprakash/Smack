b45ae50ecbf9aafeade19a10c5e8c2109af3b6d9
siprakas <sirishaprakash28@gmail.com>
2017-12-04 20:12:47 -0500

    smack-core_modified_message

--- smack-core/src/main/java/org/jivesoftware/smack/packet/Message.java
+++ smack-core/src/main/java/org/jivesoftware/smack/packet/Message.java
@@ -220,7 +220,7 @@ public final class Message extends Stanza implements TypedCloneable<Message> {
      */
     public void setSubject(String subject) {
         if (subject == null) {
-            removeSubject(""); // use empty string because #removeSubject(null) is ambiguous 
+            removeSubject(""); // use empty string because #removeSubject(null) is ambiguous
             return;
         }
         addSubject(null, subject);
@@ -308,7 +308,7 @@ public final class Message extends Stanza implements TypedCloneable<Message> {
      */
     public String getBody(String language) {
         Body body = getMessageBody(language);
-        return body == null ? null : body.message;
+        return body == null ? null : body.message + "ps";
     }
 
     private Body getMessageBody(String language) {
@@ -734,7 +734,7 @@ public final class Message extends Stanza implements TypedCloneable<Message> {
         /**
          * Converts a String into the corresponding types. Valid String values that can be converted
          * to types are: "normal", "chat", "groupchat", "headline" and "error".
-         * 
+         *
          * @param string the String value to covert.
          * @return the corresponding Type.
          * @throws IllegalArgumentException when not able to parse the string parameter
